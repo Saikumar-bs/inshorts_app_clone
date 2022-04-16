b698fec03f3404f1d6243ae9458ade42f8c5df82
SAI KUMAR MISALA <saikumar@bluesapience.com>
2022-04-16 16:50:29 +0530

    added news text

--- lib/news_screen.dart
+++ lib/news_screen.dart
@@ -9,8 +9,8 @@ class NewsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
+      body: SafeArea(
+        child: Column(children: [
           Container(
             height: size.height * 0.4,
             decoration: BoxDecoration(
@@ -23,31 +23,45 @@ class NewsScreen extends StatelessWidget {
               ),
             ),
           ),
-          SafeArea(
-            child: Align(
-              alignment: Alignment(-0.90, -0.25),
-              child: Container(
-                height: 25.0,
-                width: 80.0,
-                decoration: BoxDecoration(
-                    color: kBackgroundColor,
-                    borderRadius: BorderRadius.circular(10)),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    'inshorts',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: kTextColor.withOpacity(0.4),
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
+          // Container(
+          //   height: 20.0,
+          //   width: 80.0,
+          //   decoration: BoxDecoration(
+          //       color: kBackgroundColor, borderRadius: BorderRadius.circular(10)),
+          //   child: Align(
+          //     alignment: Alignment.centerLeft,
+          //     child: Text(
+          //       'inshorts',
+          //       textAlign: TextAlign.center,
+          //       style: TextStyle(
+          //         color: kTextColor.withOpacity(0.45),
+          //         fontSize: 14.0,
+          //         fontWeight: FontWeight.bold,
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              'From its medieval origins to the digital era, learn everything there is to know',
+              style: TextStyle(
+                color: kTextColor,
+                fontSize: 25.0,
               ),
             ),
-          )
-        ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Ciceros De Finibus Bonorum et Malorum for use in a type specimen book.',
+              style: TextStyle(
+                color: kTextColor,
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
