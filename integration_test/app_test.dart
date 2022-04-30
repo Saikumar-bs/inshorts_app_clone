e068eb2b24166271c993cdea99775b4a7864c4c1
SAI KUMAR MISALA <saikumar@bluesapience.com>
2022-04-30 17:11:23 +0530

    final integration test - successful

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -8,57 +8,12 @@ import 'package:url_launcher/url_launcher.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  // testWidgets('PageView control test', (WidgetTester tester) async {
-  //   await tester.pumpWidget(PageView(
-  //     dragStartBehavior: DragStartBehavior.down,
-  //   ));
-
-  //   await tester.tap(find.text(
-  //       "As Delhi Sees Over 60% Spike, India's Fresh Covid Tally Zooms to 2,380"));
-  //   await tester.drag(find.byType(PageView), const Offset(-200.0, 0.0));
-  //   await tester.pump();
-
-  //   expect(
-  //       find.text(
-  //           'MS Dhoni smashes 16 runs off last 4 balls as CSK defeat MI on last ball'),
-  //       findsOneWidget);
-  // });
-
-//To check the swipe once and verify the text
-  testWidgets(
-    "check the swipe",
-    (WidgetTester tester) async {
-      await tester.pumpWidget(MyApp());
-      await Future.delayed(const Duration(seconds: 3));
-      await tester.drag(find.byType(PageView), const Offset(0, -660));
-      await Future.delayed(const Duration(seconds: 3));
-      await tester.pump();
-
-      expect(find.text(kDetails2), findsOneWidget);
-    },
-  );
-
-// To check the swipe twice and verify the text
-  // testWidgets(
-  //   "check the swipe for 2 pages",
-  //   (WidgetTester tester) async {
-  //     await tester.pumpWidget(MyApp());
-  //     await Future.delayed(const Duration(seconds: 1));
-  //     await tester.drag(find.byType(PageView), const Offset(0, -660));
-  //     await Future.delayed(const Duration(seconds: 1));
-  //     await tester.drag(find.byType(PageView), const Offset(0, -660));
-  //     await Future.delayed(const Duration(seconds: 1));
-  //     await tester.drag(find.byType(swipeDirection(details.delta.dx < 0)));
-  //     await tester.pump();
-
-  //     expect(find.text, 'Hindustan Times');
-  //   },
-  // );
-
-  testWidgets("Pan working check", (WidgetTester tester) async {
+// To check the swipe down twice, swipe up once and then right swipe to open link
+  testWidgets("Complete integration test", (WidgetTester tester) async {
     String? swipeDirection;
+    Offset? panDelta;
 
-    _launchURL() async {
+    Future<void> _launchURL() async {
       const url = 'https://www.hindustantimes.com';
       if (await canLaunch(url)) {
         await launch(url);
@@ -74,51 +29,32 @@ void main() {
       }
     }
 
-    await tester.pumpWidget(SizedBox.expand(
-      child: GestureDetector(
-        onPanUpdate: (
-          DragUpdateDetails details,
-        ) {
-          updatePosition(details);
-        },
-        child: Container(),
-      ),
-    ));
-    await tester.pump();
+    await tester.pumpWidget(MyApp());
+    await Future.delayed(const Duration(seconds: 5));
+    await tester.drag(find.byType(PageView), const Offset(0, -660));
+    await Future.delayed(const Duration(seconds: 5));
+    await tester.drag(find.byType(PageView), const Offset(0, -660));
+    await Future.delayed(const Duration(seconds: 5));
+    await tester.drag(find.byType(PageView), const Offset(0, 660));
+    await Future.delayed(const Duration(seconds: 5));
 
+    await tester.pumpWidget(GestureDetector(
+      onPanUpdate: (DragUpdateDetails details) {
+        panDelta = details.delta;
+        print('Swiped to left');
+        if (panDelta!.dx == 0) {
+          print('Swiped to left');
+        } else {
+          _launchURL();
+        }
+      },
+    ));
     expect(swipeDirection, isNull);
 
-    // await tester.dragFrom(const Offset(250, 350), const Offset(850, 350));
-    await tester.drag(find.byType(GestureDetector), const Offset(440, 0),
-        warnIfMissed: false);
-    await Future.delayed(const Duration(seconds: 6));
+    await tester.pumpAndSettle();
 
-    // expect(swipeDirection, 'left');
+    await tester.drag(find.byType(GestureDetector), const Offset(-440, 0));
+
+    // expect(find.text('Home'), findsOneWidget);
   });
-
-  testWidgets(
-    "Pan swipe check",
-    (WidgetTester tester) async {
-      Offset? panDelta;
-      String? swipe;
-      await tester.pumpWidget(GestureDetector(
-        onPanUpdate: (DragUpdateDetails details) {
-          panDelta = details.delta;
-          swipe = panDelta
-              .toString(); // Neglect this (Trying to make a method for function)
-
-          print('Swiped to left');
-        },
-      ));
-      expect(panDelta, isNull);
-      // await tester.dragFrom(const Offset(200, 0.0), const Offset(440.0, 0.0));
-      await tester.pumpWidget(MyApp());
-      await Future.delayed(const Duration(seconds: 3));
-      await tester.drag(find.byType(PageView), const Offset(0, -660));
-      await Future.delayed(const Duration(seconds: 3));
-      await tester.drag(find.byType(GestureDetector), const Offset(440, 0));
-      await Future.delayed(const Duration(seconds: 6));
-      expect(panDelta!.dx, 420.00000000000006);
-    },
-  );
 }
